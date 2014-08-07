f98f8bc844a59c2834093494990370a90658ffb3
wubian1215 <mail@sorvey.com>
2014-08-07 17:27:50 +0800

    mod userModel

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -18,4 +18,22 @@ var userShema = new schema({
 	}
 })
 
+userShema.pre('save', function(next){
+	if(this.isNew){
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	}else{
+		this.meta.updateAt = Date.now();
+	}
+	next();
+})
+
+userShema.statics = {
+	fetch: function(cb){
+
+	},
+	findById: function(id, cb){
+		return this.findOne({_id: id}).exec(cb);
+	}
+}
+
 module.exports = userShema;
